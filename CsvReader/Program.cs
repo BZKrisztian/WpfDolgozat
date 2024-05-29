0abdb3078530818edbe20536f0a8a521605031db
Szakallas Kocsog <cross@HK-47>
2024-05-29 13:53:46 +0200

    Add project files.

---
+++ CsvReader/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using CsvReader.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CsvReader
+{
+    public class Program
+    {
+        static void Main(string[] args) {
+            string path2File = "C:/Temp/2.csv";
+            string catchName = "James Bond";
+            Connector4Dtb.ImportFromCsv(path2File, catchName);
+        }
+
+    }
+}
---
+++ WpfDolgozat/Tasks/Page3.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using CsvReader.Data.Models;
+using CsvReader.Data;
+
+namespace WpfDolgozat.Tasks
+{
+    /// <summary>
+    /// Interaction logic for Page3.xaml
+    /// </summary>
+    public partial class Page3 : UserControl
+    {
+        public Page3(string catchName)
+        {
+            InitializeComponent();
+            LoadData(catchName);
+        }
+
+        private void LoadData(string catchName)
+        {
+            using (var context = new Context())
+            {
+                List<CsvData> data = context.CsvData_stuff
+                    .Where(d => d.Name == catchName)
+                    .ToList();
+                databoxFromCsv.ItemsSource = data;
+            }
+        }
+    }
+}
